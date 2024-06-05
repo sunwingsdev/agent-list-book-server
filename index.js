6439e5c143cf29023839f178d2420ca857efc37e
simanto97 <saimumsimanto97@gmail.com>
2024-06-05 18:28:09 +0600

    npm start added 2

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cors(corsConfig));
 app.options("", cors(corsConfig));
 app.use(express.json());
 
-const uri = process.env.DB_URI;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1xm2m6f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
