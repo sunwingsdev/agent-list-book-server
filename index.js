8abffa25a3577856d24c7b1d873926ca84a0a782
simanto97 <saimumsimanto97@gmail.com>
2024-06-06 12:53:37 +0600

    modify again

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ require("dotenv").config();
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 
-// Import modules
+// import modules
 const logoApi = require("./apis/logoApi/logoApi");
 const homeContentsApi = require("./apis/homeContentsApi/homeContentsApi");
 
@@ -16,13 +16,12 @@ const corsConfig = {
   methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
 };
 
-// ===== Middlewares =====
+// =====middlewares======
 app.use(cors(corsConfig));
 app.options("", cors(corsConfig));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1xm2m6f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
-
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -32,35 +31,37 @@ const client = new MongoClient(uri, {
   },
   useNewUrlParser: true,
   useUnifiedTopology: true,
+  tls: true, // Ensure TLS/SSL is enabled
+  tlsCAFile: "/path/to/ca-cert.pem", // Optional: Path to CA certificate if needed
+  tlsAllowInvalidCertificates: true,
 });
 
 async function run() {
   try {
-    // Connect the client to the server
+    // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
-    console.log("MongoDB Connected ✅");
 
-    // Collection start
+    // collection start
     const logosCollection = client.db("agent-list-book").collection("logos");
     const homeContentsCollection = client
       .db("agent-list-book")
       .collection("homeContents");
 
-    // APIs start
+    // collection end
+
+    // Apis start
     app.use("/logos", logoApi(logosCollection));
     app.use("/home-contents", homeContentsApi(homeContentsCollection));
-    // APIs end
+    // Apis end
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-  } catch (err) {
-    console.error("Error connecting to MongoDB:", err);
+    console.log("MongoDB Connected ✅");
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
   }
 }
-
 run().catch(console.dir);
 
 // =====basic set up=======
