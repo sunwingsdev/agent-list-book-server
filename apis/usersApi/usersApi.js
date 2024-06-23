e1720ee80acad3b1a0363723569585022442543a
simanto97 <saimumsimanto97@gmail.com>
2024-06-23 18:28:21 +0600

    user post api added

--- apis/usersApi/usersApi.js
+++ apis/usersApi/usersApi.js
@@ -5,7 +5,12 @@ const usersApi = (usersCollection) => {
   // add user
   userRouter.post("/", async (req, res) => {
     const userInfo = req.body;
-    // const foundResult=await usersCollection.findOne({})
+    const foundResult = await usersCollection.findOne({ uid: userInfo.uid });
+    if (foundResult) {
+      return res.status(404).send({ message: "user already added" });
+    }
+    const result = await usersCollection.insertOne(userInfo);
+    res.send(result);
   });
 
   return userRouter;
